84fb97b8ebb8e24d57063781b03a9984b87531e9
Florian-Seiller <florian.seiller@uha.fr>
2026-05-29 01:28:27 +0200

    Update vueAccueil.php

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -3,6 +3,6 @@ $titre = "Accueil - Épiphanoff & Co.";
 ?>
 
 <div class="accueil">
-    <h2>BAISE TA MERE GROS FILS DE PUTE DE MERDE </h2>
+    <h2>Bienvenue sur Boutique Épiphanoff&Co</h2>
     <p>Page d'accueil temporaire. Utilisez le menu pour naviguer.</p>
 </div>
